51f45a6fc6b9b385b84eee59e65091783c9516b9
RaenProger <49145202+RaenProger@users.noreply.github.com>
2019-11-07 11:50:09 +0300

    practice 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-prompt("Введите URL")
-var url = "https://yandex.ru"
-url.location = "https://moscoding.ru"
-console.log(url);
+//Практика 2
+var money = prompt("Cколько у вас с собой есть денег?");
+var apples = prompt("Cколько вы купили яблок?");
+var bread = prompt("Cколько вы купили батонов хлеба?");
+var appleprice = prompt("Cколько стоит одно яблоко?");
+var breadprice = prompt("Cколько стоит один батон хлеба?");
+
+var total = money == appleprice + breadprice;
+console.log(total);
+
+
