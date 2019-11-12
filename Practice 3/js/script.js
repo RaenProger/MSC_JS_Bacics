6f4e58cb1e1b0d77e3a97370f19425ca8d45f7e2
RaenProger <49145202+RaenProger@users.noreply.github.com>
2019-11-12 12:35:16 +0300

    practice 3 quest_3

--- Practice 3/js/script.js
+++ Practice 3/js/script.js
@@ -2,10 +2,27 @@
 
 
 //Practice 3
+
+//Question -1
+
 const Body = document.querySelector('body');
 
-let quest = prompt('Какой ты хошечшь фон у страницы');
+let quest = prompt('Какой ты хошечшь фон у страницы?');
 Body.style.backgroundColor = quest;
+
+//Question -2
+
+const Page = document.querySelector('.page');
+
+let quest2 = prompt('Какой будет цвет текста на странице?');
+Page.style.backgroundColor = quest2;
+
+//Question -3
+let quest3 = prompt('Как зовут человека, который вас вдохновляет?');
+const Name = document.querySelector('.name');
+Name.setAttribute('name', '');
+
+
 
 
 
