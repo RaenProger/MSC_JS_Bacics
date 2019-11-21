584ab8dcc1bce7ca3bba1187fd27ea87c0d7908a
RaenProger <49145202+RaenProger@users.noreply.github.com>
2019-11-21 11:32:20 +0300

    4.4.1(ver. 1)

---
+++ 04.4.1. ternar operator/js/script.js
@@ -0,0 +1,36 @@
+//Тернарный оператор Задание 4.4.1
+/*
+const Image = document.querySelector('.imageBlock');
+let answer = prompt('Сколько ему лет');
+answer = parseInt(answer);
+
+(answer >= 18) ? 
+Image.style.backgroundImage = 'url(img/item1.png)': 
+Image.style.backgroundImage = 'url(img/item7.png)';
+*/
+
+const rock = 0;
+const scissors = 1;
+const paper = 2;
+
+// попросим игрока ввести ответ в виде числа
+let player = parseInt(prompt('Your choice: 0 - rock, 1 - scissors, 2 - paper'));
+// Рандомное число 0, 1 или 2 — разберем это позже, пока не заморачивайтесь
+let computer = Math.floor(Math.random() * 3);
+
+// опишем все условия и будем выводить в консоль 'computer win' или 'player win'
+
+//Game
+player == 0 && computer == 1 ?
+console.log('player win'):
+console.log('computer win');
+
+player == 1 && computer == 2 ?
+console.log('player win'):
+console.log('computer win');
+
+player == 2 && computer == 0 ?
+console.log('player win'):
+console.log('computer win');
+
+
