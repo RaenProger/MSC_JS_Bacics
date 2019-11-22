b1a20d397f5bbddca1d142c1e039247e52e722c3
RaenProger <49145202+RaenProger@users.noreply.github.com>
2019-11-22 13:54:14 +0300

    04.8.1 Practice (ver1)

---
+++ 04.8.1 Practice/js/script.js
@@ -0,0 +1,17 @@
+
+//Практика 2
+var money = prompt("Cколько у вас с собой есть денег?");
+var apples = prompt("Cколько вы купили яблок?");
+var bread = prompt("Cколько вы купили батонов хлеба?");
+var applePrice = prompt("Cколько стоит одно яблоко?");
+var breadPrice = prompt("Cколько стоит один батон хлеба?");
+var totalPrice = applePrice * apples + breadPrice * bread;
+var total = money >= totalPrice;
+//console.log(total);
+
+function haveEnough(money, apples, bread, applePrice, breadPrice){
+    return money, apples, bread, applePrice, breadPrice;
+}
+
+alert(haveEnough('Вам хватает денег на покупки'));
+alert(haveEnough('Вам не хватает денег'));
