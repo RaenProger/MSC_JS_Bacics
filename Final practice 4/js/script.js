3f6e6a9403c68060b04863434395797276a215ac
RaenProger <49145202+RaenProger@users.noreply.github.com>
2019-11-27 23:53:20 +0300

    Final practice(ver 2)

--- Final practice 4/js/script.js
+++ Final practice 4/js/script.js
@@ -18,9 +18,33 @@ https://jsfiddle.net/Lsk3qopt/
 4. ﻿По нажатию на элемент .closeMenu — изменит свойство left у .menu обратно на строку ‘-50vw’
 */
 
+// 1. 2.
 const button = document.querySelector('button');
 const popUp = document.querySelector('.popup');
 
+let state = 'none';
 button.addEventListener('click', function(){
+    if (state == 'none'){
     popUp.style.display = 'flex';
+    state = 'flex';
+    } else {
+    popUp.style.display = 'none';
+    state = 'none';
+    }
 });
+
+// 3.4.
+const openMenu = document.querySelector('.openMenu');
+const menu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.closeMenu');
+
+openMenu.addEventListener('click' , function(){
+if(state == 'none'){
+menu.style.left = '0';
+state = '0';
+} else {
+closeMenu.style.left = '-50vw';
+state = '-50vw';
+}
+});
+
